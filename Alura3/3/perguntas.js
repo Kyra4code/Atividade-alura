e1478bb991cfe6c94874c139227acaa44ef4edfb
Vitor Guilherme R. <162996534+Kyra4code@users.noreply.github.com>
2024-11-25 13:26:40 -0300

    atividade3 do alura feita

---
+++ Alura3/3/perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Programação',
+    'O que é Golang?',
+    'O Python é uma linguagem de programação'
+);
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+);
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+);
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigado em Japonês',
+    'Oi em ingles é Arigato-Gozaimasu'
+);
+
+//Posso fazer em JSON se pá.
